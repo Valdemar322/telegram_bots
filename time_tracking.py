a340a3624bc3680c9585b8fada8642839d89342f
Valdemort <lavrovvoldemar@gmail.com>
2023-11-14 18:02:24 +0200

    first work version calendar functionality

--- time_tracking.py
+++ time_tracking.py
@@ -35,6 +35,7 @@ days_of_week = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"]
 count_last_month = 0
 count_month = 0
 count_year = 0
+year = 0
 
 
 @bot.message_handler(commands=['help', 'start'])
@@ -105,6 +106,8 @@ def echo_message(message):
                          reply_markup=markup_days)
         global count_month
         count_month = date_today.month
+        global year
+        year = date_today.year
         global count_last_month
         count_last_month = 0
         global count_year
@@ -113,26 +116,29 @@ def echo_message(message):
 
 @bot.callback_query_handler(func=lambda callback: True)
 def month_generator(callback):
+    global count_month
+    global count_year
+    global count_last_month
+    global year
+
     if callback.data == "last_month":
         bot.delete_message(callback.message.chat.id, callback.message.message_id)
         markup_days = types.InlineKeyboardMarkup(row_width=len(days_of_week))
-        global count_last_month
+
         count_last_month += 1
         temp_days_week = []
-        date_today = date.today()
-
-        global count_month
-        global count_year
 
         if count_month - count_last_month < 1:
             count_year += 1
             count_last_month = 0
             count_month = 12
-            first_day_of_month = calendar.monthrange(date_today.year - count_year, count_month - count_last_month)[0]
-            last_day_of_month = calendar.monthrange(date_today.year - count_year, count_month - count_last_month)[-1]
+            first_day_of_month = calendar.monthrange(year - count_year, count_month - count_last_month)[0]
+            last_day_of_month = calendar.monthrange(year - count_year, count_month - count_last_month)[-1]
+            year -= 1
+
         else:
-            first_day_of_month = calendar.monthrange(date_today.year - count_year, count_month - count_last_month)[0]
-            last_day_of_month = calendar.monthrange(date_today.year - count_year, count_month - count_last_month)[-1]
+            first_day_of_month = calendar.monthrange(year, count_month - count_last_month)[0]
+            last_day_of_month = calendar.monthrange(year, count_month - count_last_month)[-1]
 
         with_range = 7
         height_range = 6
@@ -160,8 +166,66 @@ def month_generator(callback):
                 btn_days_placeholder.append(temp_str.split("-"))
                 temp_str = ""
 
-        print(res)
-        print(btn_days_placeholder)
+        for day in days_of_week:
+            temp_days_week.append(types.InlineKeyboardButton(f"{day}", callback_data=f"{day}"))
+
+        for week_num in btn_days_placeholder:
+            for day_num in week_num:
+                temp_days_week.append(types.InlineKeyboardButton(f"{day_num}", callback_data=f"{day_num}"))
+
+        btn_last_month = types.InlineKeyboardButton("< (last month)", callback_data="last_month")
+        btn_next_month = types.InlineKeyboardButton("(next month) >", callback_data="next_month")
+        markup_days.add(*temp_days_week)
+        markup_days.row(btn_last_month, btn_next_month)
+
+        bot.send_message(callback.message.chat.id,
+                         f"Выбери дату в календаре:\n Текущая дата: {count_month - count_last_month}.{year}",
+                         reply_markup=markup_days)
+
+    if callback.data == "next_month":
+
+        bot.delete_message(callback.message.chat.id, callback.message.message_id)
+        markup_days = types.InlineKeyboardMarkup(row_width=len(days_of_week))
+        count_last_month -= 1
+        temp_days_week = []
+
+        if count_month - count_last_month > 12:
+            count_year -= 1
+            count_last_month = 0
+            count_month = 1
+            first_day_of_month = calendar.monthrange(year - count_year, count_month - count_last_month)[0]
+            last_day_of_month = calendar.monthrange(year - count_year, count_month - count_last_month)[-1]
+            year += 1
+
+        else:
+            first_day_of_month = calendar.monthrange(year, count_month - count_last_month)[0]
+            last_day_of_month = calendar.monthrange(year, count_month - count_last_month)[-1]
+
+        with_range = 7
+        height_range = 6
+        res = ""
+        temp = 0
+        date_placeholder = 1
+
+        for y in range(height_range):
+            for x in range(temp, temp + with_range):
+                if x < first_day_of_month or x >= last_day_of_month + first_day_of_month:
+                    res += " " + "-"
+                else:
+                    res += str(date_placeholder) + "-"
+                    date_placeholder += 1
+            res += "\n"
+            temp += with_range
+
+        btn_days_placeholder = []
+        temp_str = ""
+        for _ in res:
+            temp_str += _
+            if _ == "\n":
+                temp_str = temp_str.rstrip()
+                temp_str = temp_str[:-1]
+                btn_days_placeholder.append(temp_str.split("-"))
+                temp_str = ""
 
         for day in days_of_week:
             temp_days_week.append(types.InlineKeyboardButton(f"{day}", callback_data=f"{day}"))
@@ -176,11 +240,8 @@ def month_generator(callback):
         markup_days.row(btn_last_month, btn_next_month)
 
         bot.send_message(callback.message.chat.id,
-                         f"Выбери дату в календаре:\n Текущая дата: {count_month - count_last_month}.{date_today.year - count_year}",
+                         f"Выбери дату в календаре:\n Текущая дата: {count_month - count_last_month}.{year}",
                          reply_markup=markup_days)
-
-    if callback.data == "next_month":
-        bot.delete_message(callback.message.chat.id, callback.message.message_id)
 
 
 bot.infinity_polling()
